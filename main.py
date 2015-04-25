94c6381b25bad04317729f9155c8912d016bb2a6
Tyler Wright <tylwright@gmail.com>
2015-04-24 21:46:08 -0400

    Exit fix

--- main.py
+++ main.py
@@ -25,18 +25,39 @@ def addHostDHCPDNS():
 		mac = raw_input("MAC Address: ")
 		ip = raw_input("IP: ")
 		subprocess.call(["fab -f fab_functions.py addHost:%s,%s,%s --hosts=%s --user=%s --password=%s" % (ip,mac,hostname, serverName, serverUser, serverUserPass)], shell=True)
-		addHost = raw_input("%s, Would you like to add another DHCP reservation/DNS record to %s? [Y/N]" % (serverUser, serverName))
-		addHost = addHost.lower()
-		if addHost == "y":
-			addHost = True
-		else:
-			addHost = False
-			mainMenu()
+	addHost = raw_input("%s, Would you like to add another DHCP reservation/DNS record to %s? [Y/N]" % (serverUser, serverName))
+	addHost = addHost.lower()
+	if addHost == "y":
+		addHost = True
+	else:
+		addHost = False
+		mainMenu()
 
 def viewDHCPReservations():
-		serverName, serverUser, serverUserPass = getWindowsCredentials()
-		scopeId = raw_input("What scope would you like to look at? ")
-		subprocess.call(["fab -f fab_functions.py getIPv4Reservations:%s --hosts=%s --user=%s --password=%s" % (scopeId, serverName, serverUser, serverUserPass)], shell=True)
+	serverName, serverUser, serverUserPass = getWindowsCredentials()
+	scopeId = raw_input("What scope would you like to look at? ")
+	subprocess.call(["fab -f fab_functions.py getIPv4Reservations:%s --hosts=%s --user=%s --password=%s" % (scopeId, serverName, serverUser, serverUserPass)], shell=True)
+
+def getVMsList():
+	serverName, serverUser, serverUserPass = getWindowsCredentials()
+	subprocess.call(["fab -f fab_functions.py getVMs --hosts=%s --user=%s --password=%s" % (serverName, serverUser, serverUserPass)], shell=True)
+
+
+def askExitToMenu():
+	exitToMenu = raw_input("Return to main menu [Y] or quit [Q]? ")
+	exitToMenu = exitToMenu.lower()
+	if exitToMenu == "q":
+		choice = "q"
+	else:
+		choice = "1111"
+	return choice
+
+def goodbye():
+	# Print the goodbye message
+	print "\nGoodbye!\n"
+	sleep(0.5)
+	clearScreen()
+	exit()
 
 
 def mainMenu():
@@ -65,31 +86,19 @@ def mainMenu():
 		choice = raw_input("Choice: ")
 		choice = choice.lower()
 		clearScreen()
+		if choice == "q":
+			goodbye()
 		if choice == "1":
 			addHostDHCPDNS()
-			exitToMenu = raw_input("Return to main menu [Y] or quit [Q]? ")
-			exitToMenu = exitToMenu.lower()
-			if exitToMenu == "q":
-				choice = "q"
 		if choice == "4":
 			viewDHCPReservations()
-			exitToMenu = raw_input("Return to main menu [Y] or quit [Q]? ")
-			exitToMenu = exitToMenu.lower()
-			if exitToMenu == "q":
-				choice = "q"
 		if choice == "5":
-			serverName = raw_input("Windows Server IP or Hostname: ")
-			serverUser = raw_input("Windows Server Username: ")
-			subprocess.call(["fab -f fab_functions.py getVMs --hosts=%s --user=%s" % (serverName, serverUser)], shell=True)
+			getVMsList()
 		if choice == "6":
 			hostname = raw_input("IP or Hostname: ")
 			user = raw_input("Username: ")
 			service = raw_input("Service name to restart: ")
 			subprocess.call(["fab -f fab_functions.py restartServiceLinux:%s --hosts=%s --user=%s" % (service, hostname, user)], shell=True)
-	
-	# Print the goodbye message
-	print "\nGoodbye!\n"
-	sleep(0.5)
-	clearScreen()
+		choice = askExitToMenu()
 	
 mainMenu()
